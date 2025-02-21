f67ddcf44ca67b69493c64a392b2bf889f49c55a
Girish <girish.ikhe.vu@gmail.com>
2025-02-21 13:39:21 +0530

    all dependencies are installed

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
